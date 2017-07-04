467b9a6c42f123b778566a2028f6fa8779bdb739
Madeline Collier <madel@telus.net>
2017-07-04 23:21:16 +0000

    fixed bug so that new_urls form actually displays

--- express_server.js
+++ express_server.js
@@ -19,8 +19,8 @@ app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls", (req, res) => {
@@ -39,6 +39,10 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.post("/urls", (req, res) => {
