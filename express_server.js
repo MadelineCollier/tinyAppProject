24e9936d100acbc2f25a546ca2268a37d0909dd3
Madeline Collier <madel@telus.net>
2017-07-04 21:40:38 +0000

    installed body-parser, and updated express server variables

--- express_server.js
+++ express_server.js
@@ -1,14 +1,16 @@
 //tinyAppProject/express_server.js
 
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true})); //allows us to access POST request parameters
+
+app.set("view engine", "ejs"); //tells the express app to use EJS as its templating engine
 
 
-app.set("view engine", "ejs"); //tells the express app to use EJS as its templating engine.
-
-
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
