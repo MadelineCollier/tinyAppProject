1d6d7f011cb7c6434b03dcee04c22e7f6ee0d9b1
Madeline Collier <madel@telus.net>
2017-07-04 22:00:42 +0000

    updated comments, cleaning up

--- express_server.js
+++ express_server.js
@@ -1,54 +1,54 @@
-// //tinyAppProject/express_server.js
+//tinyAppProject/express_server.js
 
-// const express = require("express");
-// const app = express();
-// const PORT = process.env.PORT || 8080; // default port 8080
-// const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
 
-// app.use(bodyParser.urlencoded({extended: true})); //allows us to access POST request parameters
+app.use(bodyParser.urlencoded({extended: true})); //allows us to access POST request parameters
 
-// app.set("view engine", "ejs"); //tells the express app to use EJS as its templating engine
+app.set("view engine", "ejs"); //tells the express app to use EJS as its templating engine
 
 
-// const urlDatabase = {
-//   "b2xVn2": "http://www.lighthouselabs.ca",
-//   "9sm5xK": "http://www.google.com"
-// };
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
-// app.get("/", (req, res) => {
-//   res.end("Hello!");
-// });
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
 
-// app.get("/hello", (req, res) => {
-//   res.end("<html><body>Hello <b>World</b></body></html>\n");
-// });
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
 
-// app.get("/urls", (req, res) => {
-//   let templateVars = { urls: urlDatabase }; //variables we can access in urls_index
-//   res.render("urls_index", templateVars);
-// });
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase }; //variables we can access in urls_index
+  res.render("urls_index", templateVars);
+});
 
-// app.get("/urls/:id", (req, res) => {
-//   let templateVars = { shortURL: req.params.id, urls: urlDatabase }; //variables we can access in urls_show
-//   res.render("urls_show", templateVars);
-// });
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id, urls: urlDatabase }; //variables we can access in urls_show
+  res.render("urls_show", templateVars);
+});
 
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
 
-// app.get("/urls/new", (req, res) => {
-//   res.render("urls_new");
-// });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
-// app.post("/urls", (req, res) => {
-//   console.log(req.body);  // debug statement to see POST parameters
-//   res.send("Ok");         // Respond with 'Ok' (we will replace this)
-// });
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
-// app.listen(PORT, () => {
-//   console.log(`Example app listening on port ${PORT}!`);
-// });
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
 
 
@@ -56,10 +56,5 @@
 const generateRandomString = () => {
   return Math.random().toString(36).substr(2,6);
 };
-
-console.log(generateRandomString());
-
-
-
 
 
